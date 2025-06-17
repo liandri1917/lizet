41b424847632d7e97b3c912b2b947049c96cc650
Lizet y Andrea <lizetarevalo23@gmail.com>
2025-06-17 07:42:06 -0500

    Version 002

--- lizet/bin/lizet.dart
+++ lizet/bin/lizet.dart
@@ -1,5 +1,9 @@
 import 'package:lizet/lizet.dart' as lizet;
 
 void main(List<String> arguments) {
-  print('Hello world: ${lizet.calculate()}!');
+  mensaje("Hola mundo");
+}
+
+void mensaje(String texto) {
+  print(texto);
 }
